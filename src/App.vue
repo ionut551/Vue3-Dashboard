e891ff926f1e61e3fbffee37410b13adf0e734c5
ionut551 <ionutdragomir551@gmail.com>
2022-07-18 22:47:30 -0400

    added sidenav

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
+  <!-- <Sidenav /> -->
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+// import Sidenav from './components/Sidenav'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    // Sidenav
   }
 }
 </script>
